406c07ac73cdc1291585463d6b6a7e78a34222f9
ayaanqui <moahammedayaan.dev@gmail.com>
2020-04-21 15:24:17 -0500

    Added edit product routes

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ const adminController = require('../controllers/admin');
 // GET request for add-product
 router.get('/add-product', adminController.getAddProduct);
 router.post('/add-product', adminController.postAddProduct);
+router.get('/edit-product/:id', adminController.getEditProduct);
+router.post('/edit-product', adminController.postEditProduct);
 router.get('/products', adminController.getProducts);
 
 module.exports = router;
