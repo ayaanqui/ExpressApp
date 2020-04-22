4c88c971f7078df61ab5bd47c1d7435ee3e57b04
ayaanqui <moahammedayaan.dev@gmail.com>
2020-04-22 10:11:18 -0500

    Added instructions for db password

--- util/database.js
+++ util/database.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql2');
+// Make sure to add password.js file in the util folder
+// password.js: exports.password = 'password';
 const password = require('./password');
 
 const pool = mysql.createPool({
