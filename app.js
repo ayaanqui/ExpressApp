3448f221d0f491bf3210190aef4f9db46dc297b9
ayaanqui <moahammedayaan.dev@gmail.com>
2020-05-12 23:37:37 -0500

    Product to User relation

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 
 // MySQL Database
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 // Pug templating engine
 app.set('view engine', 'pug');
@@ -31,7 +33,10 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errors.get404);
 
-sequelize.sync()
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize.sync({ force: true })
   .then(res => {
     console.log('\nServer started at http://localhost:3000/\n\n');
     app.listen(3000);
