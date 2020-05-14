487092c0b6de1cea9649aa9980648f6c0e349ad9
ayaanqui <moahammedayaan.dev@gmail.com>
2020-05-14 13:13:04 -0500

    Added req.user as first user

--- app.js
+++ app.js
@@ -26,6 +26,15 @@ const errors = require('./controllers/errors');
 /** Middleware */
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,8 +45,18 @@ app.use(errors.get404);
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
-sequelize.sync({ force: true })
+sequelize
+  .sync()
   .then(res => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Ayaan', email: 'email@gmail.com' })
+    }
+    return user;
+  })
+  .then(user => {
     console.log('\nServer started at http://localhost:3000/\n\n');
     app.listen(3000);
   })
