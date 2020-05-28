67454096ab63fa37482f936ad5e7e38db27b0c25
ayaanqui <moahammedayaan.dev@gmail.com>
2020-05-27 20:50:21 -0500

    Order and OrderItem models

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cartItem');
+const Order = require('./models/order');
+const OrderItem = require('./models/orderItem');
 /**
  * Models relations
  */
@@ -23,6 +25,11 @@ Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
+// Orders
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+Product.belongsToMany(Order, { through: OrderItem });
 
 // Pug templating engine
 app.set('view engine', 'pug');
