2741a3e92883d80114dc2f883e01c0e9074dcffc
Ayaan Siddiqui <ayaanqui@pop-os.localdomain>
2020-06-10 22:19:39 -0500

    New User model

--- models/user.js
+++ models/user.js
@@ -1,15 +1,20 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING,
-});
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
+
+  save() {
+    return getDb().collection('users').insertOne(this);
+  }
+
+  static findByPk(id) {
+    return getDb().collection('users')
+      .findOne({ _id: mongodb.ObjectId(id) });
+  }
+}
 
 module.exports = User;
