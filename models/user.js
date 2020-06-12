7006624aa8077aa43df71c5e3d129d095a2df6f0
Ayaan Siddiqui <ayaanqui@pop-os.localdomain>
2020-06-11 22:18:01 -0500

    Removed old cart models and replaced with cart within user model

--- models/user.js
+++ models/user.js
@@ -2,13 +2,26 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart) {
     this.username = username;
     this.email = email;
+    this.cart = cart; // {items: []}
   }
 
   save() {
     return getDb().collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cartProduct => {
+    //   return cartProduct._id === product._id;
+    // });
+
+    const updatedCart = { item: [{ ...product, quantity: 1 }] };
+    return getDb().collection('users').updateOne(
+      { id: new mongodb.ObjectId(this._id) },
+      { $set: { cart: updatedCart } }
+    );
   }
 
   static findByPk(id) {
